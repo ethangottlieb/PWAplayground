b5efcdbece8eeec508a87502f944f068ea6b6eef
Ethan Gottlieb <etgottli@microsoft.com>
2018-08-09 15:34:08 -0700

    phising shhh

--- react.js
+++ react.js
@@ -15,9 +15,15 @@ function tick() {
       name= "Jeff";
       }
   const element = React.createElement('h1', null, name);
-
+      document.getElementById('square').style.color = "blue";
+      document.getElementById('square').style.fillOpacity = 1;
   // highlight-next-line
   ReactDOM.render(element, document.getElementById('root'));
-}
-
+  //ReactDOM.render(gameBoard, document.getElementById('square'));
+    }
 setInterval(tick, 1000);
+
+function ethanClickFunction(){
+    console.log("fuck yeah");
+    window.location.href="phising.html";
+}
